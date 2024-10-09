3234e74be5c568393ecbae1d32df3bf21a23e0ef
Izanagi95 <xiachengha95@gmail.com>
2024-10-09 21:26:29 +0200

    refactor

--- jenkins_home/war/scripts/hudson-behavior.js
+++ jenkins_home/war/scripts/hudson-behavior.js
@@ -2582,9 +2582,6 @@ function buildFormTree(form) {
           if (e.classList.contains("complex-password-field")) {
             addProperty(p, "$redact", shortenName(e.name));
           }
-          if (e.classList.contains("secretTextarea-redact")) {
-            addProperty(p, "$redact", shortenName(e.name));
-          }
           break;
       }
     }
